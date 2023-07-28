b1f15a67420019134ec2698568e89d139b544501
Prasetyo Nur Hidayat <prasetyonurhidayat@gmail.com>
2023-07-29 06:18:03 +0700

    init

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize('al_iman_boga', 'root', '', {
-  host: 'localhost',
+const db = new Sequelize('bcxfg7iydd7d8edy26ca', 'ux0o1eposvsql6ky', '0u4U4g4ULbHHp21cr8WQ', {
+  host: 'bcxfg7iydd7d8edy26ca-mysql.services.clever-cloud.com',
   dialect: 'mysql'
 });
 
