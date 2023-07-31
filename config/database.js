dc2ee4ba60c0f3658a2e5f5af1aee74519c20663
Prasetyo Nur Hidayat <prasetyonurhidayat@gmail.com>
2023-08-01 02:20:17 +0700

    add mysql2

--- config/database.js
+++ config/database.js
@@ -1,8 +1,21 @@
 import { Sequelize } from 'sequelize';
+import mysql2 from 'mysql2'; // Needed to fix sequelize issues with WebPack
+
 
 const db = new Sequelize('bcxfg7iydd7d8edy26ca', 'ux0o1eposvsql6ky', '0u4U4g4ULbHHp21cr8WQ', {
+  
   host: 'bcxfg7iydd7d8edy26ca-mysql.services.clever-cloud.com',
-  dialect: 'mysql'
+  dialect: 'mysql',
+  dialectModule: mysql2, // Needed to fix sequelize issues with WebPack
+  
 });
+export async function connectToDatabase() {
+  console.log('Trying to connect via sequelize')
+  await db.sync()
+  await db.authenticate()
+  console.log('=> Created a new connection.')
+
+  // Do something 
+}
 
 export default db;
